d4be0064984e125c97deb90ec4600da2ce4b8e75
version-1 <j.onodera26@gmail.com>
2020-05-21 09:31:16 -0700

    add table sections

--- iOS-Assignement6/ViewController.swift
+++ iOS-Assignement6/ViewController.swift
@@ -8,15 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController, UITableViewDragDelegate , UITableViewDropDelegate{
+    
+    
+    
+    struct PropertyKeys {
+        static let todoCell = "TodoCell"
+    }
+    
+    let headers = ["High Priority", "Medium Priority", "Low Priority"]
     
     let label = UILabel()
-    var list: [Todo] = []
+    var list: [[Todo]] = [
+        [Todo(id: 1, title: "hoge", priority: 1, status: 0)],
+        [Todo(id: 2, title: "fuga", priority: 1, status: 0)],
+        [Todo(id: 3, title: "page", priority: 1, status: 0)]
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setHeader()
         setBody()
+        
+        tableView.dragInteractionEnabled = true
+        tableView.dragDelegate = self
+        tableView.dropDelegate = self
     }
     
     func setHeader() {
@@ -29,10 +46,40 @@ class ViewController: UIViewController {
     
     func setBody() {
         view.backgroundColor = UIColor.white
-        label.text = "Hello World"
-        label.textAlignment = .center
-        view.addSubview(label)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: PropertyKeys.todoCell)
+        tableView.allowsMultipleSelectionDuringEditing = true
     }
+    
+    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        return []
+    }
+    
+    func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list[section].count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+      return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+       return headers[section]
+    }
+    
+    // create a cell for each table view row
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: PropertyKeys.todoCell, for: indexPath)
+        
+        // set the text from the data model
+        cell.textLabel?.text = self.list[indexPath.section][indexPath.row].title
+        cell.accessoryType = .detailButton
+        
+        return cell
+    }
+    
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -40,7 +87,7 @@ class ViewController: UIViewController {
     }
     
     @objc func onTapLeftItem() {
-        
+        tableView.setEditing(true, animated: true)
     }
     
     @objc func onTapDelete() {
